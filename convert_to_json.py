62062f94fa6f497395a47aa78d1ef4ee7654b447
David Gurr <davidgurr@fb.com>
2020-01-13 14:21:31 -0500

    more things

--- convert_to_json.py
+++ convert_to_json.py
@@ -2,25 +2,28 @@ import fileinput
 import sys
 import re
 
+def convert_file_to_json(file):
+    i = 0
+    with fileinput.input(files=(file)) as f:
+        for line in f:
+            i += 1
+        f.close()
+
+    p = re.compile(r'.*(ObjectId\((\"\w+\")\)).*')
+
+    with fileinput.input(files=(file), inplace=False) as f:
+        for line in f:
+            if (f.isfirstline()):
+                line = line.replace('{', '[{')
+            if (f.lineno() == i):
+                line = line.replace('}', '}]')
+            else:
+                line = line.replace('}', '},')
+            match = p.match(line)
+            if match is not None and len(match.groups()) > 0:
+                line = line.replace(match.groups()[0], match.groups()[1])
+            print(line, end='')
+
 file = sys.argv[1]
-i = 0
-with fileinput.input(files=(file)) as f:
-    for line in f:
-        i += 1
-    f.close()
-
-p = re.compile(r'.*(ObjectId\((\"\w+\")\)).*')
-
-with fileinput.input(files=(file), inplace=False) as f:
-    for line in f:
-        if (f.isfirstline()):
-            line = line.replace('{', '[{')
-        if (f.lineno() == i):
-            line = line.replace('}', '}]')
-        else:
-            line = line.replace('}', '},')
-        match = p.match(line)
-        if match is not None and len(match.groups()) > 0:
-            line = line.replace(match.groups()[0], match.groups()[1])
-        print(line, end='')
+convert_file_to_json(file)
             
