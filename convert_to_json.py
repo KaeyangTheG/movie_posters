c54e84dae7a8f8504cdf1ce52ad67cdf4cb4fc95
David Gurr <davidgurr@fb.com>
2020-01-13 22:24:28 -0500

    swing easy

--- convert_to_json.py
+++ convert_to_json.py
@@ -3,16 +3,16 @@ import sys
 import re
 import os
 
-def convert_file_to_json(file):
+def convert_file_to_json(file, encoding, file2):
     i = 0
-    with fileinput.input(files=(file)) as f:
+    with fileinput.input(files=(file),openhook=fileinput.hook_encoded(encoding)) as f:
         for line in f:
             i += 1
         f.close()
 
     p = re.compile(r'.*(ObjectId\((\"\w+\")\)).*')
-
-    with fileinput.input(files=(file), inplace=False) as f:
+    f2 = open(file2, 'w')
+    with fileinput.input(files=(file), inplace=False, openhook=fileinput.hook_encoded(encoding)) as f:
         for line in f:
             if (f.isfirstline()):
                 line = line.replace('{', '[{')
@@ -23,12 +23,12 @@ def convert_file_to_json(file):
             match = p.match(line)
             if match is not None and len(match.groups()) > 0:
                 line = line.replace(match.groups()[0], match.groups()[1])
-            print(line, end='')
+            f2.write(line)
         f.close()
     
-# file = sys.argv[1]
-# convert_file_to_json(file)
 for file in os.listdir('.'):
     if (file.endswith('.txt')):
-        print(file)
-            
+        try:
+            convert_file_to_json(file, "utf-8", file.replace(".txt", ".json"))
+        except:
+            convert_file_to_json(file, "utf-16", file.replace(".txt", ".json"))
